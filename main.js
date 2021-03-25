cf590604e8b146cd4a2707798add3b958e50ce03
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-03-26 00:04:00 +0100

    init krom hudby, panacek se hýbe na button click, není porovnání mince - panáček

--- main.js
+++ main.js
@@ -8,19 +8,96 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 // sem začni psát svůj program
 
-_______________________________________________________
+let panacekX = document.querySelector('#panacek').style.left;
+let panacekY = document.querySelector('#panacek').style.top;
+const panacekSirka = 64;
+const panacekVyska = 70;
+let minceX = document.querySelector('#mince').style.left;
+let minceY = document.querySelector('#mince').style.top;
+const minceSirka = 36;
+const minceVyska = 36; 
+let score = document.querySelector('#score').innerHTML
+
+minceX =  Math.random() * (window.innerWidth - minceSirka) + 'px'
+minceY = Math.random() * (window.innerHeight - minceVyska) + 'px'
+
+console.log(window.innerWidth - panacekSirka);
+console.log(score);
 
 //init
+function init() {
 	//minceX a minceY umísti někde random
-	//panáčka zaparkuj asi na střed
+	document.querySelector('#mince').style.left = minceX;
+	document.querySelector('#mince').style.top = minceY;
+	//panáčka zaparkuj na střed
+	document.querySelector('#panacek').style.left = window.innerWidth * 0.5 - (panacekSirka * 0.5) + 'px';
+	document.querySelector('#panacek').style.top = window.innerHeight * 0.5 - (panacekSirka * 0.5) + 'px';
 	//body na nulu
+	document.querySelector('#score').innerHTML = "0";
 	//začni hrát písničku (onAnyKeyPress asi)
+	//document.querySelector('#hudba').play();
+}
+init();
+		
+/* _______________________________________________________ */
 
-_________________________________________________________
+function bod () {
+	//1. zahraj zvuk sebrání mince
+	//document.querySelector('#zvukmince').play();
+	//2. posuň minci na jiný random
+	document.querySelector('#mince').style.left = minceX;
+	document.querySelector('#mince').style.top = minceY;
+	//3. přičti bod
+	score.innerHTML = score + 1;
+}
+
+function winner () {
+	if (score == 5) {
+		//zahraj fanfáru
+		//vypiš vítěznou hlášku
+		//chtělo by to panáčka s rukama nahoře
+		//vrátit do init??
+	}
+}
+
+//pohyb panáčka
+
+function panacekRight(elementSelector, positionChange) {
+	let element = document.querySelector(elementSelector);
+	let currentPosition = parseInt(element.style.left);
+	element.style.left = (currentPosition + positionChange) + 'px';
+	element.src = "obrazky/panacek-vpravo.png";
+} 
+
+function panacekLeft(elementSelector, positionChange) {
+	let element = document.querySelector(elementSelector);
+	let currentPosition = parseInt(element.style.left);
+	element.style.left = (currentPosition - positionChange) + 'px';
+	element.src = "obrazky/panacek-vlevo.png";
+} 
+
+function panacekUp(elementSelector, positionChange) {
+	let element = document.querySelector(elementSelector);
+	let currentPosition = parseInt(element.style.top);
+	element.style.top = (currentPosition - positionChange) + 'px';
+	element.src = "obrazky/panacek-nahoru.png";
+} 
+
+function panacekDown(elementSelector, positionChange) {
+	let element = document.querySelector(elementSelector);
+	let currentPosition = parseInt(element.style.top);
+	element.style.top = (currentPosition + positionChange) + 'px';
+	element.src = "obrazky/panacek.png";
+} 
+
+
+/* _______________________________________________________ */
+
+
 
 //onkeydown (pravá šipka):
 
-//if panáček dojde k minci, tj. pozice panáček == pozice mince (nápověda nahoře)
+//if panáček je na minci, tj. pozice panáček == pozice mince (nápověda nahoře)
 	//1. zahraj zvuk sebrání mince
 	//2. posuň minci na jiný random
 	//3. přičti bod	
@@ -36,15 +113,15 @@ _________________________________________________________
 		//1. vyměň obrázek za koukající do prava
 		//2. přičti k panacekX nějakou inteligentní hodnotu (bacha, ať nezdrhne z okna  0 - window.innerWidth)
 		
-__________________________________________________________
+/* _____________________________________________________ */
 
 
 //onkeydown (levá šipka):
-//if dojde k minci, else: panáček doleva
+//if je na minci, else: panáček doleva
 
 //onkeydown (šipka nahoru):
-//if dojde k minci, else: panáček nahoru
+//if je na minci, else: panáček nahoru
 
 //onkeydown(šipka dolů):
-//if dojde k minci, else: panáček dolů
+//if je na minci, else: panáček dolů
 
